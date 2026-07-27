8e3045f4daa1cb8a9ac8106e32182a00e63c8c3c
DimLoong <81123765+DimLoong@users.noreply.github.com>
2026-07-27 14:35:17 +0800

    update info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://neoforge-docs-cn.pages.dev",
+    url: "https://neoforge-docs-cn.dimloong.workers.dev/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
